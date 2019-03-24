42dbe4e4fcac2a11c401cff3fd9ac928a556c792
Dmitry Yakimenko <detunized@gmail.com>
2019-03-24 10:56:11 +0100

    Bot embryo with config parsing

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+// Config represents the structure of the config.json file
+type Config struct {
+	Token string `json:"token"`
+}
+
+func readConfig() Config {
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer file.Close()
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	var config Config
+	err = json.Unmarshal(bytes, &config)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return config
+}
+
+func main() {
+	config := readConfig()
+
+	bot, err := tgbotapi.NewBotAPI(config.Token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = false
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(updateConfig)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	for update := range updates {
+		// Ignore any non-Message Updates
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		// Reply
+		text := fmt.Sprintf("> %s", update.Message.Text)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, text)
+
+		bot.Send(msg)
+	}
+}
